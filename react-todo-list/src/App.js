ab1a4ff1ec0160da79c9e9533b94e32e3bffb322
southAndy <zatchbell.1206@gmail.com>
2022-11-02 17:14:13 +0800

    refactor: relayout page

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -2,7 +2,12 @@ import React,{useState,useEffect,useMemo} from 'react';
 import './App.css';
 import Nav from "./components/Nav";
 import TodoList from './components/TodoList';
-import './sass/style.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+//fontawesome 的icon 資料庫
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(faBars,faMagnifyingGlass)
+// import './sass/style.scss';
 
 function App() {
   
@@ -12,7 +17,13 @@ function App() {
   //專門存狀態改變的
   const [stateTodo,setStateTodos] = useState("all");
 
+  
+  // let isShowSlide = false;
+  let [isShowSlide,setSlideStatus] = useState(false);
+  
+
   const renderTodo = useMemo(()=>{
+    setSlideStatus(false)
     return todos.filter((todo)=>{
       switch(stateTodo){
         case 'doing':
@@ -26,24 +37,37 @@ function App() {
 
   },[stateTodo,todos])
   useEffect(()=>{
-    //todo 從local 讀取資料
-  },[todos])
+    //https://jsonplaceholder.typicode.com/
+    fetch('https://jsonplaceholder.typicode.com/todos').then((res)=>res.json()).then((json)=>setTodo(json))
+  },[])
+  
+  let tepm = ()=>{
+    isShowSlide = !isShowSlide
+    setSlideStatus(isShowSlide)
+  }
   
   
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Todo-list</h1>
-        <Nav input={input} setInput={setInput} todos={todos} setTodo={setTodo}/>
-        <nav>
-          <p onClick={()=>setStateTodos('all')}>全部待辦事項</p>
+      <section className="App-main">
+        <header className="App-header">
+          <h1>Todo-list</h1>
+          <FontAwesomeIcon icon="fa-bars" onClick={()=>tepm()}/>
+        </header>
+          <Nav input={input} setInput={setInput} todos={todos} setTodo={setTodo}/>
+        <TodoList todos={renderTodo} setTodo={setTodo}/>
+      </section>
+      <nav className={`navbar ${isShowSlide?'navbar-show':'navbar-none'}`}>
+          <p onClick={()=>setStateTodos('all')}>全部</p>
           <p onClick={()=>setStateTodos('doing')}>進行中</p>
           <p onClick={()=>setStateTodos('done')}>完成</p>
-        </nav>
-      </header>
-      <TodoList todos={renderTodo} setTodo={setTodo}/>
-      
+          {/* fixed */}
+          <p>
+            <FontAwesomeIcon icon="fa-magnifying-glass" />
+          </p>
+      </nav>
     </div>
+    
   );
 }
 
